6462afafe56c53259725e9f5faa17e55347911ff
PedroMPagani <pedro.paulo.pagani@gmail.com>
2023-06-05 20:50:30 -0300

    latest code for http server.

---
+++ backend/Controller/src/main/java/org/fluxosquad/Client.java
@@ -0,0 +1,38 @@
+package org.fluxosquad;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Client {
+
+    public void testRequest(){
+        try {
+            URL url = new URL("http://localhost:8080/get"); // Replace with the actual server URL
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                System.out.println("Response:");
+                System.out.println(response);
+            } else {
+                System.out.println("Request failed with response code: " + responseCode);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- backend/Controller/src/main/java/org/fluxosquad/Main.java
+++ backend/Controller/src/main/java/org/fluxosquad/Main.java
@@ -1,7 +1,27 @@
 package org.fluxosquad;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args){
+        System.out.println("Init controller = RestAPI.");
+        Server server = new Server();
+        Thread thread = new Thread(()->{
+            try(Scanner scanner = new Scanner(System.in)) {
+                while (true) {
+                    String line = scanner.nextLine();
+                    System.out.println(line);
+                }
+            }
+
+        });
+        thread.setPriority(Thread.MIN_PRIORITY);
+        thread.setDaemon(true);
+        thread.start();
+        server.start();
+        Client client = new Client();
+        client.testRequest();
     }
+
 }
