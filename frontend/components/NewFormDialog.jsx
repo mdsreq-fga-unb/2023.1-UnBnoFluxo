b9126e144ea603cf22981a51d6d2dbf361a6736a
Ricardo <lrsj2003@gmail.com>
2023-06-10 07:38:24 -0300

    mesclar funcional

--- frontend/components/NewFormDialog.jsx
+++ frontend/components/NewFormDialog.jsx
@@ -8,55 +8,65 @@ import {
     Input,
     TextField,
 } from "@mui/material"
+import axios from "axios"
 import React, { useEffect, useState } from "react"
 
 export default function NewFormDialog({ open, onClose, addData }) {
+    // Estados
     const [coursesList, setCoursesList] = useState(null) // Estado para armazenar as opcoes de fluxograma existentes na api
     const [selectedCourses, setSelectedCourses] = useState(null) // Estado para armazenar os cursos selecionados
     const [selectedFile, setSelectedFile] = useState(null) // Estado para armazenar arquivo selecionado
 
-    const handleAssemble = () => {
-        // Criando funcao assincrona que vai fazer o fetch na api // TODO: colocar isso em um try catch
-        const fetchData = async () => {
-            // const response = await fetch("135.148.35.38:25532/get") // TODO: fazer o uso da api
-            const response = await fetch(`/api/${selectedCourses.endpoint}`)
-            const data = await response.json()
-            addData(data)
+    // Funcao assincrona para o fetch na api de cursos disponiveis
+    const fetchCourses = async () => {
+        try {
+            // const response = await axios.get("135.148.35.38:25532/get/courses") // TODO: fazer o uso da api
+            const response = await axios.get("/api/courses")
+            setCoursesList(response.data)
+        } catch (error) {
+            console.error("Erro ao obter dados da API:", error)
         }
-
-        if (selectedFile) {
-            addData(selectedFile)
-        } else if (selectedCourses) {
-            fetchData() // Chamando funcao assincrona
-        } else {
-            alert("Faça upload de um arquivo ou selecione uma das opcoes do nosso banco de dados")
-        }
-        onClose() // Fecha o dialogo apos montar novo fluxograma
     }
 
+    // Funcao que cuida da atualizacao dos dados atualmente montados na aplicacao
+    const handleAssemble = () => {
+        if (selectedFile) {
+            addData(selectedFile)
+            setSelectedFile(null) // Limpa o estado do arquivo após a montagem
+        }
+
+        if (selectedCourses) {
+            selectedCourses.forEach(async (selectedCourse) => {
+                try {
+                    // const response = await axios.get(`135.148.35.38:25532/get/${selectedCourse.endpoint}`) // TODO: fazer o uso da api
+                    const response = await axios.get(`/api/${selectedCourse.endpoint}`)
+                    addData(response.data)
+                } catch (error) {
+                    console.error(`Erro ao obter dados do curso "${selectedCourse.name}":`, error)
+                }
+            })
+            setSelectedCourses(null) // Limpa o estado dos cursos selecionados após a montagem
+        }
+        onClose()
+    }
+
+    // Funcao que atualiza os dados do estado que monitora quais cursos estao autalmente selecionados no Autocomplete
     const handleFileChange = (event) => {
         const file = event.target.files[0]
 
         // Verifica se o arquivo é do tipo JSON
         if (file && file.type === "application/json") {
             const reader = new FileReader()
-
             reader.onload = (e) => {
                 try {
-                    // Lê o conteúdo do arquivo
-                    const content = e.target.result
-
-                    // Converte o conteúdo do arquivo em uma lista de objetos
-                    const data = JSON.parse(content)
-
-                    // Atualiza o estado com a lista de objetos
-                    setSelectedFile(data)
+                    const content = e.target.result // Lê o conteúdo do arquivo
+                    const data = JSON.parse(content) // Converte o conteúdo do arquivo em uma lista de objetos
+                    setSelectedFile(data) // Atualiza o estado com a lista de objetos
                 } catch (error) {
                     console.error("Erro ao converter arquivo JSON:", error)
                     setSelectedFile(null) // Limpa o estado em caso de erro de conversão
                 }
             }
-
             reader.readAsText(file)
         } else {
             console.error("Formato de arquivo inválido. Selecione um arquivo JSON.")
@@ -64,15 +74,9 @@ export default function NewFormDialog({ open, onClose, addData }) {
         }
     }
 
+    // useEffect que contem as funcoes que sao executadas sempre que o componte NewFormDialog é reindenizado
     useEffect(() => {
-        // TODO: Usar axios pra fazer o fetch
-        const fetchData = async () => {
-            const response = await fetch("/api/courses")
-            const data = await response.json()
-            setCoursesList(data)
-        }
-
-        fetchData()
+        fetchCourses()
     }, [])
 
     return (
@@ -83,12 +87,11 @@ export default function NewFormDialog({ open, onClose, addData }) {
                 <p>Para montar seu fluxograma você escolha uma das opções abaixo:</p>
                 {coursesList && (
                     <Autocomplete
-                        // multiple // TODO: MUDAR ESSE PARAMETRO PARA PERMITIR MESCLAR
+                        multiple
                         id="tags-outlined"
                         options={coursesList}
                         getOptionLabel={(course) => course.name}
                         filterSelectedOptions
-                        value={selectedCourses} // Valor dos cursos selecionados
                         onChange={(event, newValue) => {
                             setSelectedCourses(newValue)
                         }} // Atualiza o estado dos cursos selecionados
