46038f549ac045bf887b17bda56f3670e540e208
vini051 <60819460+vini051@users.noreply.github.com>
2023-06-12 19:33:06 -0300

    Update Card.jsx

--- frontend/components/Card.jsx
+++ frontend/components/Card.jsx
@@ -1,9 +1,12 @@
 import styles from "../styles/Card.module.css"
 
 export default function Card({ course }) {
+
     return (
         <div className={styles.card} title={course.code + " / " + course.period + "º Per"}>
             <strong>{course.name}</strong>
+            <div className={`${styles.tipo}  ${course.nature=="optative"?styles.optative:styles.mandatory}`}></div>
         </div>
     )
+
 }
