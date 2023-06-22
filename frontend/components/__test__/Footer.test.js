9a2c8ddeb1103392e256a1989e65ce5d942dc50c
vini051 <vini051150@gmail.com>
2023-06-22 02:35:57 -0300

    configured ci file to run tests

--- frontend/components/__test__/Footer.test.js
+++ frontend/components/__test__/Footer.test.js
@@ -1,10 +1,9 @@
 import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect'
 import Footer from '../Footer'
 
 it('should render the footer with repo linked into text', async () => {
-    render(<Footer />);
-    const linkElement = screen.getByRole('link', { name: /Equipe • Documentação • Direitos/i });
-    linkElement.setAttribute('href', 'https://example.com');
-    expect(linkElement).toHaveAttribute('href', 'https://example.com');
+    render(<Footer />)
+    const linkElement = screen.getByRole('link', { name: /Equipe • Documentação • Direitos/i })
+    expect(linkElement).toHaveAttribute('href', 'https://github.com/mdsreq-fga-unb/2023.1-UnBnoFluxo')
 });
