d458de4fa960343287ec92bc3e4131d9b86ac0d1
Ricardo <lrsj2003@gmail.com>
2023-06-24 20:13:46 -0300

    ajustando cards para texto com nome do curso

--- frontend/components/Card.jsx
+++ frontend/components/Card.jsx
@@ -2,12 +2,19 @@ import styles from "../styles/Card.module.css"
 
 export default function Card({ course }) {
     return (
-        <div className={styles.card} title={course.code + " / " + course.period + "º Per"}>
-            <strong>{course.alias}</strong>
+        <div
+            className={styles.card}
+            title={course.displayName + "\n" + course.code + " / " + course.period + "º Per"}
+        >
+            <div className={styles.textBox}>
+                {course.alias ? (
+                    <strong className={styles.alias}>{course.alias}</strong>
+                ) : (
+                    <strong className={styles.displayName}>{course.displayName}</strong>
+                )}
+            </div>
             <div
-                className={`${styles.tipo}  ${
-                    course.nature == "OBRIGATORIO" ? styles.mandatory : styles.optative
-                }`}
+                className={`${course.nature == "OBRIGATORIO" ? styles.mandatory : styles.optative}`}
             ></div>
         </div>
     )
