617ccfb26aabb0dbfff75267a4366a0f686a6532
vini051 <vini051150@gmail.com>
2023-06-29 00:41:57 -0300

    fixed customButton test - its now passing 100%

--- frontend/components/__test__/CustomButton.test.js
+++ frontend/components/__test__/CustomButton.test.js
@@ -2,25 +2,29 @@ import CustomButton from "../CustomButton"
 import { render, screen, fireEvent } from "@testing-library/react"
 import '@testing-library/jest-dom/extend-expect'
 
-// it("should render extended button variant on mouse enter", () => {
-//     render(<CustomButton text={"Teste"} />)
-//     const buttonElement = screen.getByRole('button')
-//     const typographyElement = buttonElement.querySelector('Typography')
+it("should render extended button variant with expected text on mouseEnter", () => {
+    render(<CustomButton text={'TestText'} />)
+    const buttonElement = screen.getByRole('button')
+    expect(buttonElement).toBeTruthy()
 
-//     fireEvent.mouseEnter(buttonElement)
-//     expect(buttonElement).toBeTruthy()
-//     expect(typographyElement).toBeInTheDocument()
-//   })
+    fireEvent.mouseEnter(buttonElement) //o useState tem valor default true, então esse teste passaria mesmo sem essa instrução
+   
+    const typographyElement = screen.getByText('TestText')
+    expect(typographyElement).toBeTruthy()
+    expect(buttonElement).toHaveClass('MuiFab-extended')    //https://mui.com/material-ui/api/fab/#css
+})
 
-// it("hides extended button on mouse leave", () => {
-//     render(<CustomButton text={"Teste2"} onClick={jest.fn()} />)    //depois tentar renderizar e definir a const btn fora do bloco de teste pra ver se funciona
-//     const buttonElement = screen.getByRole('button')
-
-//     fireEvent.mouseEnter(buttonElement)
-//     fireEvent.mouseLeave(buttonElement)
-
-//     expect(buttonElement).toHaveAttribute('variant', 'round')
-// })
+it("hides extended button on mouse leave", () => {
+    render(<CustomButton text={'TesteLeave'} />)
+    const buttonElement = screen.getByRole('button')
+    
+    fireEvent.mouseEnter(buttonElement)
+    fireEvent.mouseLeave(buttonElement)
+    
+    const typographyElement = screen.queryByText('TesteLeave')
+    expect(buttonElement).not.toHaveClass('MuiFab-extended')    //espera-se NÃO ter a classe MuiFab-extended após o mouseLeave
+    expect(typographyElement).toBeNull()                        //espera-se que não haja o texto TesteLeave após o mouseLeave então o queryByText retorna Null
+})
 
 it("calls onClick handler on button click", () => {
     const onClickMock = jest.fn()
@@ -29,6 +33,5 @@ it("calls onClick handler on button click", () => {
     const buttonElement = screen.getByRole('button')
 
     fireEvent.click(buttonElement)
-
     expect(onClickMock).toHaveBeenCalled()
 })
