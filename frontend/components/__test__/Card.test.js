c1950be0f8931eddb65780501fd3d427bdc14313
Ricardo <lrsj2003@gmail.com>
2023-06-30 00:18:43 -0300

    fix typo

--- frontend/components/__test__/Card.test.js
+++ frontend/components/__test__/Card.test.js
@@ -1,30 +1,31 @@
-  import Card from "../Card"
+import Card from "../Card"
 import { render, screen, waitFor } from "@testing-library/react"
-import '@testing-library/jest-dom/extend-expect'
+import "@testing-library/jest-dom/extend-expect"
 import { Button } from "@mui/material"
 
 const mockCourseObr = {
-  displayName: "TestOBRName",
-  code: "FGA0000",
-  period: 5,
-  alias: "teste",
-  nature: "OBRIGATORIO",
+    displayName: "TestOBRName",
+    code: "FGA0000",
+    period: 5,
+    alias: "teste",
+    nature: "OBRIGATORIO",
 }
 
 it("should render the card with correct information and mandatory style", async () => {
-  render(<Card course={mockCourseObr} />)
+    render(<Card course={mockCourseObr} />)
 
-  const buttonElement = screen.getByRole('button');
-  const titleElement = buttonElement.getAttribute('title');
+    const buttonElement = screen.getByRole("button")
+    const titleElement = buttonElement.getAttribute("title")
 
-  expect(titleElement).toEqual(`${mockCourseObr.displayName}\n${mockCourseObr.code} / ${mockCourseObr.period}º Per`) 
+    expect(titleElement).toEqual(
+        `${mockCourseObr.displayName}\n${mockCourseObr.code} / ${mockCourseObr.period}º Per`
+    )
 
-  const aliasElement = screen.getByText(mockCourseObr.alias)
-  const tipoElement = screen.getByTestId("tipo-element")
-  
-  expect(aliasElement).toHaveTextContent("teste")
-  expect(tipoElement).toHaveClass("mandatory")
+    const aliasElement = screen.getByText(mockCourseObr.alias)
+    const tipoElement = screen.getByTestId("tipo-element")
 
+    expect(aliasElement).toHaveTextContent("teste")
+    expect(tipoElement).toHaveClass("mandatory")
 })
 
 const mockCourseOpt = {
@@ -32,20 +33,21 @@ const mockCourseOpt = {
     code: "FGA9999",
     period: 10,
     alias: "validacao",
-    nature: "OPTATIVA",
-  }
-  
-  it("should render the card with correct information and optative style", () => {
-    render(<Card course={mockCourseOpt} />)
-  
-    const titleElement = screen.getByRole('button').getAttribute('title') //encurtei a sintaxe pra caber numa linha só
+    nature: "OPTATIVO",
+}
 
-    expect(titleElement).toEqual(`${mockCourseOpt.displayName}\n${mockCourseOpt.code} / ${mockCourseOpt.period}º Per`) 
+it("should render the card with correct information and optative style", () => {
+    render(<Card course={mockCourseOpt} />)
+
+    const titleElement = screen.getByRole("button").getAttribute("title") //encurtei a sintaxe pra caber numa linha só
+
+    expect(titleElement).toEqual(
+        `${mockCourseOpt.displayName}\n${mockCourseOpt.code} / ${mockCourseOpt.period}º Per`
+    )
 
     const aliasElement = screen.getByText(mockCourseOpt.alias)
     const tipoElement = screen.getByTestId("tipo-element")
-  
+
     expect(aliasElement).toHaveTextContent("validacao")
     expect(tipoElement).toHaveClass("optative")
-    
-  })
+})
