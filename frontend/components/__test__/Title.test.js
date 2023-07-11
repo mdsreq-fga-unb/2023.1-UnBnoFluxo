aa5c507aec24c8d8470b5b7665a196a6ef4fcfcc
joao costa <devcostta@gmail.com>
2023-07-11 11:11:06 -0300

    tirando comentario

--- frontend/components/__test__/Title.test.js
+++ frontend/components/__test__/Title.test.js
@@ -14,31 +14,31 @@ describe('Title', () => {
     expect(titleElement).toBeInTheDocument('teste')
   })
 
-  // it("calls onClick handler on Title click", () => {
-  //   const handleTitleClickMock = jest.fn();
-  //   render(<Title editable={true} onClick={handleTitleClickMock} />);
-  //   const titleElement = screen.getByText("teste");
+   it("calls onClick handler on Title click", () => {
+     const handleTitleClickMock = jest.fn();
+     render(<Title editable={true} onClick={handleTitleClickMock} />);
+     const titleElement = screen.getByText("teste");
 
-  //   fireEvent.click(titleElement);
-  //   expect(handleTitleClickMock).toHaveBeenCalled();
-  // });
+     fireEvent.click(titleElement);
+     expect(handleTitleClickMock).toHaveBeenCalled();
+   });
 
-  // it("should handle title click and change", () => {
-  //   // Arrange
-  //   const setTitle = jest.fn();
-  //   const { getByText, getByRole } = render(
-  //   <Title editable={true} title="Initial Title" setTitle={setTitle} />
-  //   );
+   it("should handle title click and change", () => {
+     // Arrange
+     const setTitle = jest.fn();
+     const { getByText, getByRole } = render(
+     <Title editable={true} title="Initial Title" setTitle={setTitle} />
+     );
     
-  //   const titleElement = getByText("Initial Title");
+     const titleElement = getByText("Initial Title");
     
-  //   // Act
-  //   fireEvent.click(titleElement);
-  //   const inputElement = getByRole("textbox");
-  //   fireEvent.change(inputElement, { target: { value: "New Title" } });
-  //   fireEvent.blur(inputElement);
+     // Act
+     fireEvent.click(titleElement);
+     const inputElement = getByRole("textbox");
+     fireEvent.change(inputElement, { target: { value: "New Title" } });
+     fireEvent.blur(inputElement);
     
-  //   // Assert
-  //   expect(setTitle).toHaveBeenCalledWith("New Title");
-  //   });
+     // Assert
+     expect(setTitle).toHaveBeenCalledWith("New Title");
+     });
 })
