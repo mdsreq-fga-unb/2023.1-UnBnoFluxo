e8412a798c9e5a93b8d2c5fe619226ae8e3686c0
vini051 <vini051150@gmail.com>
2023-07-12 02:50:59 -0300

    tip test fully working

--- frontend/components/__test__/Tip.test.js
+++ frontend/components/__test__/Tip.test.js
@@ -5,33 +5,36 @@ import "@testing-library/jest-dom/extend-expect"
 /* UNDER DEVELOPMENT */
 
 describe ('Tip', () => {
-
-    it('should render Tip with "#DB3B4B" bgcolor when errorMode is true', () => {
-        render(<Tip text={'TesteTrue'} errorMode={ true } />)
+    it('should render Tip with correct bgc and errorMsg when errorMessage is defined', () => {
+        render(<Tip errorMessage={{ message: 'TestErrorMsg' }} />)
 
         const boxElement = screen.getByTestId('tipo-box')                                        //query do <Box/>
         expect(boxElement).toHaveStyle({ background: "#DB3B4B" })
+        expect(boxElement).toHaveTextContent('TestErrorMsg')                                     //testa se ta renderizando a mensagem de erro
+        expect(boxElement).not.toHaveTextContent('AnyThinElse')                                  //testa se não renderiza qualquer outra coisa que não a mensagem de erro
     })
 
-    it('should render Tip with "#232323" bgcolor when errorMode is undefined', () => {
-        render(<Tip text={'TesteFalse'} errorMode={ undefined } />)
+    it('should render Tip with correct bgc and text when errorMessage is UNdefined', () => {
+        render(<Tip text={'TesteFalse'} />)
 
         const boxElement = screen.getByTestId('tipo-box')
         expect(boxElement).toHaveStyle({ background: "#232323" })
+        expect(boxElement).toHaveTextContent('TesteFalse')
+        expect(boxElement).not.toHaveTextContent('AnyThinElse')
     })    
 
-    // it('should render ReportSharpIcon when isErrorMode is true', () => {
-    //     render(<Tip text={'TesteTrue'} errorMode={ true } />)
+    it('should render ReportSharpIcon when errorMessage is true', () => {
+        render(<Tip text={'TesteTrue'} errorMessage={{ message: 'TestErrorMsg' }} />)
 
-    //     const reportIcon = screen.getByRole('img', { name: /report icon/i })                    //query do reportIcon
-    //     expect(reportIcon).toBeInTheDocument()
-    // })
+        const reportIcon = screen.getByTestId('ReportSharpIcon')                      //query do reportIcon - descobri esse testid inspecionando no browser,
+        expect(reportIcon).toBeInTheDocument()                                        //entao pelo visto alguns componentes do  muimaterial ja tem essa prop pra teste
+    })
 
-    // it('should render HelpSharpIcon when isErrorMode is undefined', () => {
-    //     render(<Tip text={'TesteFalse'} errorMode={ undefined } />)
+    it('should render HelpSharpIcon when errorMessage is undefined', () => {
+        render(<Tip text={'TesteFalse'} errorMessage={ undefined } />)
 
-    //     const helpIcon = screen.getByRole('img', { name: /help icon/i })                        //query do helpIcon
-    //     expect(helpIcon).toBeInTheDocument()
-    // })
+        const helpIcon = screen.getByTestId('HelpSharpIcon')                         //query do helpIcon
+        expect(helpIcon).toBeInTheDocument()
+    })
 })
     
