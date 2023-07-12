aa37a4c2f56908599c9d5884042b3dcc31deea7b
vini051 <vini051150@gmail.com>
2023-07-12 02:28:29 -0300

    tip test

--- frontend/components/__test__/Tip.test.js
+++ frontend/components/__test__/Tip.test.js
@@ -5,19 +5,33 @@ import "@testing-library/jest-dom/extend-expect"
 /* UNDER DEVELOPMENT */
 
 describe ('Tip', () => {
-    it('should render Tip with "#DB3B4B" bg color when errorMode is true', () => {
-        render (<Tip text={'TesteTrue'} errorMode={false} />)
 
-    //     const boxElement = screen.getByTestId('tip-box')
-    //     expect(boxElement).toHaveStyle({background: "#DB3B4B"})        
+    it('should render Tip with "#DB3B4B" bgcolor when errorMode is true', () => {
+        render(<Tip text={'TesteTrue'} errorMode={ true } />)
+
+        const boxElement = screen.getByTestId('tipo-box')                                        //query do <Box/>
+        expect(boxElement).toHaveStyle({ background: "#DB3B4B" })
+    })
+
+    it('should render Tip with "#232323" bgcolor when errorMode is undefined', () => {
+        render(<Tip text={'TesteFalse'} errorMode={ undefined } />)
+
+        const boxElement = screen.getByTestId('tipo-box')
+        expect(boxElement).toHaveStyle({ background: "#232323" })
+    })    
+
+    // it('should render ReportSharpIcon when isErrorMode is true', () => {
+    //     render(<Tip text={'TesteTrue'} errorMode={ true } />)
+
+    //     const reportIcon = screen.getByRole('img', { name: /report icon/i })                    //query do reportIcon
+    //     expect(reportIcon).toBeInTheDocument()
     // })
 
-    // it('should render Tip with "#232323" bg color when errorMode is false', () => {
-    //     render (<Tip text={'TesteFalse'} errorMode={false} />)
+    // it('should render HelpSharpIcon when isErrorMode is undefined', () => {
+    //     render(<Tip text={'TesteFalse'} errorMode={ undefined } />)
 
-    //     const boxElement = screen.getByLabelText('TesteFalse')
-    //     expect(boxElement).toHaveStyle({background: "#232323"})        
-    })
-    
+    //     const helpIcon = screen.getByRole('img', { name: /help icon/i })                        //query do helpIcon
+    //     expect(helpIcon).toBeInTheDocument()
+    // })
 })
     
