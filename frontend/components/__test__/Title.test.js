76099d609f892cc1590b65760c16e19dc436455e
vini051 <vini051150@gmail.com>
2023-07-12 01:48:33 -0300

    removed "under development" status from title test

--- frontend/components/__test__/Title.test.js
+++ frontend/components/__test__/Title.test.js
@@ -2,13 +2,11 @@ import Title from "../Title"
 import { render, screen, fireEvent } from "@testing-library/react"
 import '@testing-library/jest-dom/extend-expect'
 
-/* UNDER DEVELOPMENT */
-
 describe('Title', () => {
   it ("should render *editable* Title", () => {
     render (<Title title = {"teste"}/> )
-    const titleElement = screen.getByText('teste')      //query do titulo
-    fireEvent.click(titleElement)                      //simula click no titulo
+    const titleElement = screen.getByText('teste')                       //query do titulo
+    fireEvent.click(titleElement)                                        //simula click no titulo
     expect(titleElement).toBeInTheDocument()
 
   })
@@ -19,15 +17,15 @@ describe('Title', () => {
     <Title editable={true} title="Initial Title" setTitle={setTitle} />        
     )
     
-    const titleElement = getByText('Initial Title')                //query do title
+    const titleElement = getByText('Initial Title')                     //query do title
     
-    fireEvent.click(titleElement)                                 //simula click
+    fireEvent.click(titleElement)                                       //simula click
     const inputElement = getByRole('textbox')
     fireEvent.change(inputElement, { target: { value: "New Title" } })  //altera o texto para 'New Title'
     fireEvent.blur(inputElement)
     
-    expect(setTitle).toHaveBeenCalledWith('New Title')            //testa o setTitle
+    expect(setTitle).toHaveBeenCalledWith('New Title')                  //testa o setTitle
 
-    expect(titleElement).toHaveTextContent('New Title')            //confere se titulo é 'New Title'
+    expect(titleElement).toHaveTextContent('New Title')                 //confere se titulo agora é 'New Title'
     })
 })
