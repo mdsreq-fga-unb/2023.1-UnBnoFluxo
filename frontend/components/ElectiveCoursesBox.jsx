6c906e418e248ca782349a8483138121fc6b50d3
joao costa <devcostta@gmail.com>
2023-07-14 22:25:51 -0300

    adicionando botao de pesquisa

--- frontend/components/ElectiveCoursesBox.jsx
+++ frontend/components/ElectiveCoursesBox.jsx
@@ -2,13 +2,40 @@ import NewCard from "./NewCard"
 import Card from "./Card"
 import styles from "../styles/ElectiveCoursesBox.module.css"
 import { Box, CircularProgress, Grid } from "@mui/material"
+import React, { useState } from "react";
 
 export default function ElectiveCoursesBox({ data, addData }) {
+
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    function handleSearch(event) {
+        const term = event.target.value;
+        setSearchTerm(term);
+      
+        const results = data.filter((course) => {
+          // Realize a busca pelo termo de pesquisa em campos relevantes do curso
+          const courseName = course.name.toLowerCase();
+          const courseCode = course.code.toLowerCase();
+          return courseName.includes(term.toLowerCase()) || courseCode.includes(term.toLowerCase());
+        });
+      
+        setSearchResults(results);
+      }
+      
+
     return (
         <Box className={styles.box}>
             <Box className={styles.tilte_box}>
                 <h2>OPTATIVAS</h2>
             </Box>
+
+            <input
+                type="text"
+                placeholder="Pesquisar..."
+                value={searchTerm}
+                onChange={handleSearch}
+            />
 
             <Grid
                 container
