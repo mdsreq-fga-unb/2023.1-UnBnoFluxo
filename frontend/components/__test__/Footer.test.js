0e556cae935070243f0a3c599038348e5c775a2f
Ricardo <lrsj2003@gmail.com>
2023-07-18 18:05:52 -0300

    melhorando footer

--- frontend/components/__test__/Footer.test.js
+++ frontend/components/__test__/Footer.test.js
@@ -1,9 +1,22 @@
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import Footer from '../Footer'
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom/extend-expect"
+import Footer from "../Footer"
 
-it('should render the footer with repo linked into text', () => {
+it("should render the footer with correct links", () => {
     render(<Footer />)
-    const linkElement = screen.getByRole('link', { name: /Equipe • Documentação • Direitos/i })
-    expect(linkElement).toHaveAttribute('href', 'https://github.com/mdsreq-fga-unb/2023.1-UnBnoFluxo')
-});
+    const linkElement1 = screen.getByRole("link", { name: /Equipe/i })
+    expect(linkElement1).toHaveAttribute(
+        "href",
+        "https://mdsreq-fga-unb.github.io/2023.1-UnBnoFluxo/#colaboradores"
+    )
+    const linkElement2 = screen.getByRole("link", { name: /Documentação/i })
+    expect(linkElement2).toHaveAttribute(
+        "href",
+        "https://mdsreq-fga-unb.github.io/2023.1-UnBnoFluxo"
+    )
+    const linkElement3 = screen.getByRole("link", { name: /Repositório/i })
+    expect(linkElement3).toHaveAttribute(
+        "href",
+        "https://github.com/mdsreq-fga-unb/2023.1-UnBnoFluxo"
+    )
+})
