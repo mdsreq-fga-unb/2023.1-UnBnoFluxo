8c37dbb3385da10ce8dbca34b64bb49812f205d5
Ricardo <lrsj2003@gmail.com>
2023-07-19 15:01:43 -0300

    fix

--- frontend/components/ElectiveCoursesBox.jsx
+++ frontend/components/ElectiveCoursesBox.jsx
@@ -1,29 +1,39 @@
 import NewCard from "./NewCard"
 import Card from "./Card"
 import styles from "../styles/ElectiveCoursesBox.module.css"
-import { Box, CircularProgress, Grid } from "@mui/material"
-import React, { useState } from "react";
+import { Box, CircularProgress, Grid, InputBase } from "@mui/material"
+import React, { useState } from "react"
+import SearchIcon from "@mui/icons-material/Search"
 
 export default function ElectiveCoursesBox({ data, addData }) {
+    const [searchText, setSearchText] = useState("") // Estado que gurda o que sta sendo pesquisado
 
-    const [searchText, setSearchText] = useState('');
+    // Funcao auxiliar para aulterar o valor do que sta sendo pesquisado
     const handleSearch = (value) => {
-        setSearchText(value);           
-    };
+        setSearchText(value.toLocaleUpperCase())
+    }
 
     return (
         <Box className={styles.box}>
             <Box className={styles.tilte_box}>
-                <h2>OPTATIVAS</h2>
-                <Box className={styles.search_box}>
-                    <h2>PESQUISAR</h2>                
+                <h2 className={styles.tilte}>OPTATIVAS</h2>
+                <Box className={styles.search}>
+                    <SearchIcon sx={{ ml: 1 }} />
+                    <InputBase
+                        sx={{
+                            m: 1,
+                            flex: 1,
+                            backgroundColor: "#FFFFFF",
+                            height: "1.5rem",
+                            padding: "0.5rem",
+                            borderRadius: "0.5rem",
+                        }}
+                        type="text"
+                        placeholder="Pesquisar componente"
+                        value={searchText}
+                        onChange={(event) => handleSearch(event.target.value)}
+                    />
                 </Box>
-                <input
-                    type="text"
-                    placeholder="Pesquisar componente"
-                    value={searchText}
-                    onChange={(event) => handleSearch(event.target.value)}
-                />
             </Box>
             <Grid
                 container
@@ -37,9 +47,13 @@ export default function ElectiveCoursesBox({ data, addData }) {
                 </Grid>
                 {data ? (
                     data
-                        .filter((course) => (course.period === 0 && searchText !== "" && (course.displayName.toLowerCase().includes(searchText.toLowerCase()) ||
-                        course.alias.toLowerCase().includes(searchText.toLowerCase()) ||
-                        course.code.toLowerCase().includes(searchText.toLowerCase())) ))
+                        .filter((course) => course.period === 0)
+                        .filter(
+                            (course) =>
+                                course.displayName.includes(searchText) ||
+                                course.alias.includes(searchText) ||
+                                course.code.includes(searchText)
+                        )
                         .map((course) => (
                             <Grid item p={2} key={course.code}>
                                 <Card course={course} data={data} addData={addData} />
